216affc01edfa0e50429dba4044a8d8d40bad336
LenaZaitseva <51196742+LenaZaitseva@users.noreply.github.com>
2019-07-07 15:20:36 +0300

    Выполняется регистрация и вход в чат

--- src/js/index.js
+++ src/js/index.js
@@ -27,8 +27,17 @@ window.onload = function() {
     /*Регистрация пользователя*/
 
     document.getElementById('btnReg').addEventListener('click', registration);
-
     function registration() {
+        autorize('https://studentschat.herokuapp.com/users/register');
+    }
+    /*Вход пользователя в чат*/
+    document.getElementById('btnEnter').addEventListener('click', enter);
+    function enter() {
+        login('https://studentschat.herokuapp.com/users');
+    }
+
+/* функция для регистрации пользователя*/
+    function autorize(who) {
         var name = document.getElementById('name').value;   console.log(name);
         if(name.length == 0) {
             var notification = document.getElementById('notification');
@@ -42,37 +51,114 @@ window.onload = function() {
                 "username": name
             };
             var request1 = new XMLHttpRequest();
-            request1.open('POST', 'https://studentschat.herokuapp.com/users/register', true);
+            request1.open('POST', who, true);
             request1.setRequestHeader('Content-Type', 'application/json');
 
-            request1.onload = function(event) {
+            request1.onload = function() {
                 // Обработчик ответа в случае удачного соеденения
-                if (this.readyState != 4 && this.status == 200) {
+                if (request1.status >= 200 && request1.status < 400) {
                     var res = JSON.parse(request1.responseText);
                     console.log(res);
                     document.getElementById('chat_wrapper').style.display = 'block';
                     document.querySelector('body').style.background = "#ffffff";
                     document.getElementById("registration-form").style.display = 'none';
-                }
-                else {
+
+                } else {
                     document.getElementById("registration-form").style.display = 'none';
                     document.querySelector('body').innerHTML =
                         '<div class="registration-form" id="modal_error">' +
-                        '<div class="modal-header">Ошибка</div>'+
+                        '<div class="modal-header">Ошибка</div>' +
                         '     <div class="modal-content">Регистрация не удалась. Попробуйте ещё раз!</div>' +
                         '     <div class="modal-buttons">' +
                         '          <input type="button" value="Ок" class="btnEnter" id="btnOk" onclick="location.reload()">' +
                         '     </div>' +
-                        '</div>'
+                        '</div>';
                 }
             }
 
             request1.onerror = function() {
-
+                document.getElementById("registration-form").style.display = 'none';
+                document.querySelector('body').innerHTML =
+                    '<div class="registration-form" id="modal_error">' +
+                    '<div class="modal-header">Ошибка</div>'+
+                    '     <div class="modal-content">Регистрация не удалась. Попробуйте ещё раз!</div>' +
+                    '     <div class="modal-buttons">' +
+                    '          <input type="button" value="Ок" class="btnEnter" id="btnOk" onclick="location.reload()">' +
+                    '     </div>' +
+                    '</div>'
             };
 
             request1.send(JSON.stringify(user));
         }
     }
+    /* функция для входа пользователя в чат*/
+    function login(who) {
+        var name = document.getElementById('name').value;   console.log(name);
+        if(name.length == 0) {
+            var notification = document.getElementById('notification');
+
+            notification.style.display = "block";
+            notification.innerText += "Поле не может быть пустым"
+            document.getElementById("name").style.border = "1px solid #FF3D39";
+        } else {
+            var user = {
+                "username": name
+            };
+            var request = new XMLHttpRequest();
+            request.open('GET', who, true);
+
+            request.onload = function() {
+                if (request.status >= 200 && request.status < 400) {
+                    // Обработчик успещного ответа
+                    var response = request.responseText;
+                    //var userList = [];
+
+                    JSON.parse(response).forEach(
+                        function (obj) {
+                            console.log(obj);
+                            if(obj['username'] === name) {
+                                document.getElementById('chat_wrapper').style.display = 'block';
+                                document.querySelector('body').style.background = "#ffffff";
+                                document.getElementById("registration-form").style.display = 'none';
+                            } else {
+                                document.querySelector('body').innerHTML =
+                                    '<div class="registration-form" id="modal_error">' +
+                                    '<div class="modal-header">Ошибка</div>' +
+                                    '     <div class="modal-content">Вход не выполнен. Попробуйте ещё раз!</div>' +
+                                    '     <div class="modal-buttons">' +
+                                    '          <input type="button" value="Ок" class="btnEnter" id="btnOk" onclick="location.reload()">' +
+                                    '     </div>' +
+                                    '</div>';
+                            }
+                        }
+                    )
+                } else {
+                    // Обработчик ответа в случае ошибки
+                    document.getElementById("registration-form").style.display = 'none';
+                    document.querySelector('body').innerHTML =
+                        '<div class="registration-form" id="modal_error">' +
+                        '<div class="modal-header">Ошибка</div>' +
+                        '     <div class="modal-content">Вход не выполнен. Попробуйте ещё раз!</div>' +
+                        '     <div class="modal-buttons">' +
+                        '          <input type="button" value="Ок" class="btnEnter" id="btnOk" onclick="location.reload()">' +
+                        '     </div>' +
+                        '</div>';
+                }
+            };
+            request.onerror = function() {
+                document.getElementById("registration-form").style.display = 'none';
+                document.querySelector('body').innerHTML =
+                    '<div class="registration-form" id="modal_error">' +
+                    '<div class="modal-header">Ошибка</div>' +
+                    '     <div class="modal-content">Вход не выполнен. Попробуйте ещё раз!</div>' +
+                    '     <div class="modal-buttons">' +
+                    '          <input type="button" value="Ок" class="btnEnter" id="btnOk" onclick="location.reload()">' +
+                    '     </div>' +
+                    '</div>';
+            };
+            request.send(JSON.stringify(user));
+        }
+    }
+
 
 }
